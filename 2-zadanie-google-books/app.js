5663bd5ad59cbe42b65c2db131fa89288a3600a5
15MPa <m.pajda@gmail.com>
2020-02-13 22:41:44 +0100

    wyszukiwanie, wyświetlanie w konsoli szukanego tytułu i wyświetlanie listy

--- 2-zadanie-google-books/app.js
+++ 2-zadanie-google-books/app.js
@@ -11,3 +11,45 @@ $.ajax.
 W odpowiedzi wypisz w konsoli tytuły książek.
 ------------------------------------------
 */
+$(function () {
+
+    const $bookList = $( '.list' );
+    function insertContent(books) {
+        console.log(books);
+        for (const el of books) {
+            const $li = $(`
+                <li>
+                    <h3>${el.title}</h3>
+                    <p>${el.authors}</p> 
+                </li>
+                `);
+            $bookList.append($li);
+        }
+    }
+
+
+    $('button').click(function (el) {
+        el.preventDefault();
+
+        const $data = $('#search').val();
+
+        $.ajax({
+            url: 'https://www.googleapis.com/books/v1/volumes?q=' + $data,
+
+        }).done(function (res) {
+            console.log(res);
+            for (const el of res.items){
+                console.log(el.volumeInfo.title, el.volumeInfo.authors);
+                const $li = $(`
+                <li>
+                    <h3>${el.volumeInfo.title}</h3>
+                    <p>${el.volumeInfo.authors}</p> 
+                </li>
+                `);
+                $bookList.append($li);
+            }
+
+        })
+    })
+
+});
